f085e99ee1bd72c204008db44a45e7d23a0f01b6
Chengyh8848 <cheng13467985@163.com>
2024-10-11 16:40:59 +0800

    修改日志模块结构

--- log.go
+++ log.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"fmt"
 	"github.com/natefinch/lumberjack"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -20,13 +21,39 @@ type LoggerConfig struct {
 	StdOut     bool   // 是否控制台显示
 }
 
+type Logger struct {
+	Log *zap.Logger
+}
+
 // 性能更好
-var Logger *zap.Logger
+var Log *Logger
+
+func (l *Logger) ErrorMsg(msg string, v ...interface{}) {
+	if len(v) > 0 {
+		msg = fmt.Sprintf(msg, v...)
+	}
+	l.Log.Error(msg)
+}
+
+func (l *Logger) InfoMsg(msg string, v ...interface{}) {
+	if len(v) > 0 {
+		msg = fmt.Sprintf(msg, v...)
+	}
+	l.Log.Info(msg)
+}
+
+func (l *Logger) DebugMsg(msg string, v ...interface{}) {
+	if len(v) > 0 {
+		msg = fmt.Sprintf(msg, v...)
+	}
+	l.Log.Debug(msg)
+}
 
 // 性能稍差，但是有格式宽松的结构化log，如nfow，Info，Infof；较少使用；仅使用在调用不频繁的地方
 var SugarLogger *zap.SugaredLogger
 
 func InitLogger(config LoggerConfig) {
+	Log = new(Logger)
 	// 获取日志文件句柄
 	writer := getLogWriter(config)
 	// 设置日志编码
@@ -49,14 +76,14 @@ func InitLogger(config LoggerConfig) {
 		// 开启开发者模式
 		if config.Develop {
 			development := zap.Development()
-			Logger = zap.New(core, caller, development)
+			Log.Log = zap.New(core, caller, development)
 		} else {
-			Logger = zap.New(core, caller)
+			Log.Log = zap.New(core, caller)
 		}
 	} else {
-		Logger = zap.New(core)
+		Log.Log = zap.New(core)
 	}
-	SugarLogger = Logger.Sugar()
+	SugarLogger = Log.Log.Sugar()
 	defer SugarLogger.Sync()
 }
 
